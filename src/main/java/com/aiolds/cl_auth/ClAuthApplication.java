beba6a3426782d9cb69094651b2f2fa0189e5497
taurus366 <taurus.ali47@gmail.com>
2025-11-29 20:00:05 +0200

    init

---
+++ src/main/java/com/aiolds/cl_auth/ClAuthApplication.java
@@ -0,0 +1,13 @@
+package com.aiolds.cl_auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ClAuthApplication.class, args);
+    }
+
+}
