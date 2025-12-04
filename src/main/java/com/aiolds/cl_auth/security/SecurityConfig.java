49e5d0056d14590a3db5c5229675dfe7d69fe165
taurus366 <taurus.ali47@gmail.com>
2025-12-04 23:06:50 +0200

    .

--- src/main/java/com/aiolds/cl_auth/security/SecurityConfig.java
+++ src/main/java/com/aiolds/cl_auth/security/SecurityConfig.java
@@ -5,6 +5,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
@@ -21,6 +22,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http.csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
 //                .authorizeHttpRequests(auth -> auth.anyRequest().authenticated()
 //                )
                 .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
